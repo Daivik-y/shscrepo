b3a3e4960365b6d93a6e4687892237ac985d1084
Daveyboi24 <Daivikyenduri@gmail.com>
2026-09-04 23:31:33 +0100

    guessing game with random seed

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
+  srand(time(0)); //seeding with time is needed for random number generation?? I do not like this language.
+  
   int randomNum = rand() % 101;
-  cout << randomNum <<endl;
+  //cout << randomNum <<endl;
+  
   int userguess;
   cout << "Enter your guess: ";
   cin >> userguess;
-  cout << "your guess" << userguess << endl;
+
+  while (userguess != randomNum) {
+
+    if (userguess > randomNum) {
+
+      cout << "Guess it too high: ";
+      cin >> userguess;
+
+    }
+
+  }
+  
+  cout << "your guess: " << userguess << endl;
   return 0;
 }
