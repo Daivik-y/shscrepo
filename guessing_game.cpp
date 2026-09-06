d460abd6277e743a64cc799c0e9f0c85b3725383
Daveyboi24 <Daivikyenduri@gmail.com>
2026-09-06 22:21:31 +0100

    finalver

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -1,3 +1,11 @@
+//1. No global variables. (Global constants are all right.)
+
+//2. No strings. Not ever. (You can use cstrings/character arrays, and the cstring and cctype libraries are okay. In fact, I recommend using strcmp and strlen in your projects.)
+
+//3. You should include <iostream>, not stdio. (This applies more to C programmers.) Also, use "new" and "delete" instead of "malloc" and "free".
+
+
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -5,11 +13,19 @@ using namespace std;
 
 int main()
 {
+
+  bool play = true;
+
+  while (play) {
+
+  
+  int count = 1;
+
   srand(time(0)); //seeding with time is needed for random number generation?? I do not like this language.
-  
+
   int randomNum = rand() % 101;
-  //cout << randomNum <<endl;
-  
+  //  cout << randomNum <<endl;
+
   int userguess;
   cout << "Enter your guess: ";
   cin >> userguess;
@@ -18,13 +34,33 @@ int main()
 
     if (userguess > randomNum) {
 
-      cout << "Guess it too high: ";
-      cin >> userguess;
+      cout << "Guess is too high, ";
+      cout << "Enter your guess: ";
 
+    } else if (userguess < randomNum){
+
+      cout << "Guess is too low, ";
+      cout << "Enter your guess: ";
     }
 
+    cin >> userguess;
+    count++;
   }
-  
-  cout << "your guess: " << userguess << endl;
-  return 0;
+
+  cout << "CORRECT ANSWER!!!";
+
+
+  cout << "You guessed in " << count << " tries!!!!!!" << endl;
+
+  char choice;
+  cout << "play again? (y/n)";
+  cin >> choice;
+
+  if (choice == 'n' || choice == 'N') {
+    play = false;
+    }
+
 }
+return 0;
+}
+
