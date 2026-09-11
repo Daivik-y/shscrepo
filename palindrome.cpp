d91b68d56c4655a30d48ffc76e31e31caae4334f
U-BSD\468069 <468069@SHS-CYBF6X3.beaverton.k12.or.us>
2026-09-11 13:14:01 -0700

    issue with punct loop

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 #include <cstring>
-
+#include <algorithm>
 using namespace std;
 
 int main()
 {
+  char str0[81];
   char str[81];
   char str2[81];
   
-  cin.get(str, 81);
+  cin.get(str0, 81);
   
-  //  str.Remove()
+
   
-  cout << str << endl;
+  cout << str0 << endl;
 
-  int size = strlen(str);
+     int size = strlen(str0);
 
+     int count = 0;
+     
+  for (int i = 0; i <= size-1; i++){
+    if (ispunct(str0[i])) {
+      i = i - 1;	
+    }
+
+    else{
+      str[i] = str0[i];
+    }
+    
+    count++;
+  }
+  
+  str[count+1] = '\0';
+
+  cout << "fixedstr" << str << endl;  
+      
+     
   int j = 0;
 
   for (int i = size - 1; i >= 0; i--){
@@ -26,6 +46,13 @@ int main()
   str2[j] = '\0';
 
   cout << str2 << endl;
+  int cmp = strcmp(str, str2);
+
+  if (cmp == 0) {
+    cout << str << " is a palindrome" << endl;
+  } else {
+    cout << str << " is not a palindrome" << endl;
+  }
   
   
   return 0;
